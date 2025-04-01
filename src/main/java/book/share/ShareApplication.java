4db6d237ba5b00b0e117c4740737ee14c781f808
Vitaliy F <vitaliyfilimonov3@gmail.com>
2025-04-01 12:50:31 +0300

    init commit

---
+++ src/main/java/book/share/ShareApplication.java
@@ -0,0 +1,11 @@
+package book.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApplication.class, args);
+    }
+}
